fd91b4669d4c32092aea039b5cce5bc3ed8997a8
Ilarion <138008965+Iliweqweqw@users.noreply.github.com>
2024-03-07 02:58:29 +0600

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -9,7 +9,7 @@ function greet(name) {
   if (name === "Johnny") {                  
     return "Hello, my love!";                             
   }            
-                     
+                      
   return "Hello, " + name + "!"; 
 }
 
